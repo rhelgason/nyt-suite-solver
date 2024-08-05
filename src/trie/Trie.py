721e3bc738ef715c811727c6782e16d680a2ca75
Ryan Helgason <helgasonryan@fb.com>
2024-08-04 19:16:09 -0500

    Trie to list method

--- src/trie/Trie.py
+++ src/trie/Trie.py
@@ -1,21 +1,23 @@
 from trie.Node import Node
 from typing import List
 
+END_OF_WORD = '\n'
+
 """
 Trie data structure for representing all valid words in the
 English language. For representing the end of a word, we use
-the <NEXT ASCII CHARACTER> character.
+the END_OF_WORD character.
 """
 class Trie:
     root: Node = None
     size: int = 0
 
     def __init__(self) -> None:
-        self.root = Node(None, -1)
+        self.root = Node("", -1)
         self.size = 0
 
     def add_word(self, word: str) -> None:
-        self.root = self.add_word_helper(self.root, word + '<NEXT ASCII CHARACTER>')
+        self.root = self.add_word_helper(self.root, word + END_OF_WORD)
 
     def add_word_helper(self, curr: Node, word: str) -> Node:
         # reached end of word
@@ -32,7 +34,7 @@ class Trie:
         return curr
 
     def remove_word(self, word: str) -> None:
-        self.root = self.remove_word_helper(self.root, word + '<NEXT ASCII CHARACTER>')
+        self.root = self.remove_word_helper(self.root, word + END_OF_WORD)
 
     def remove_word_helper(self, curr: Node, word: str) -> Node:
         # reached end of word
@@ -49,3 +51,18 @@ class Trie:
 
         # if child was deleted, possibly delete curr node
         return curr if any(child is not None for child in curr.children) else None
+
+    def to_list(self) -> List[str]:
+        res = []
+        self.to_list_helper(self.root, "", res)
+        return res
+    
+    def to_list_helper(self, curr: Node, word: str, words: List[str]) -> None:
+        if curr.letter == END_OF_WORD:
+            words.append(word)
+            return
+        
+        for child in curr.children:
+            if child is None:
+                continue
+            self.to_list_helper(child, word + curr.letter, words)
